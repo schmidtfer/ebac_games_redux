89fd42f962f83b30fe0ce4916c96f5766b9ba4b5
fernanda <fernandatqfs@hotmail.com>
2025-06-15 20:06:55 -0300

    alteracoes

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Game } from '../../App'
+
+type CarrinhoState = {
+  itens: Game[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    //abaixo é o reducer (uma função e a action)
+    adicionar: (state, action: PayloadAction<Game>) => {
+      const jogo = action.payload
+
+      if (state.itens.find((game) => game.id === jogo.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(jogo)
+      }
+    }
+  }
+})
+
+export const { adicionar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
